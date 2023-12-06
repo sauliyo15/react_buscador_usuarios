78c24f1213383dabcefa766459b65a4b74ec98e0
sauliyo15 <gc.saul.7@gmail.com>
2023-12-06 12:08:49 +0100

    Omitimos el uso de Formulario para pasar los tests

--- src/App.js
+++ src/App.js
@@ -1,59 +1,63 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-import Header from './Header';
-import Formulario from './Formulario.js';
-import Resultados from './Resultados';
-import {mock1} from './constants/users.js';
-import CONFIG from './config/config.js';
+import Header from "./Header";
+//import Formulario from "./Formulario.js";
+import Resultados from "./Resultados";
+import { mock1 } from "./constants/users.js";
+import CONFIG from "./config/config.js";
 
 function App() {
-
   const [query, setQuery] = useState("");
   const [resultado, setResultado] = useState(null);
   const [error, setError] = useState(null);
 
-
   const callServer = async (param) => {
-    
+
     if (CONFIG.use_server) {
       try {
         let queryparams;
+        
         if (param === "all") {
           queryparams = "?limit=" + CONFIG.num_items;
-        }
+        } 
         else {
           queryparams = "/search?q=" + query;
         }
+
         const response = await fetch(CONFIG.server_url + queryparams);
         const data = await response.json();
+        
         setResultado(data.users);
-        setError(null);        
+        setError(null);
+
       } catch (error) {
         console.log(error);
-        setError({description: error.message});
+        setError({ description: error.message });
       }
-    }
-    else {
+    } else {
       setResultado(mock1.users);
-      setError(null);  
+      setError(null);
     }
-  }
+  };
 
   return (
-    <div id='main'>
-      <Header/>
+    <div id="main">
+      <Header />
+      <h2 id="buscador">Buscador de usuarios</h2>
       
-      <h2 id='buscador'>Buscador de usuarios</h2>
+      <div>
+        <input type="text" id="query" placeholder="Texto a buscar" value={query} onChange={e=>setQuery(e.target.value)}></input>
+      </div>
+			<br/>
       
-      <Formulario contenido={query} setContenido={setQuery} callServer={callServer}/>
-
-      <button id='botonall' onClick={()=>callServer("all")}>Buscar Todos</button>
-
+      <button id="botonsearch" className="new" onClick={()=>callServer()}>Buscar</button>
+      
+      <button id="botonall" className="new" onClick={()=>callServer("all")}>Ver Todos</button>        		
+      
       {error && <h1>Ha habido un error: {error.description}</h1>}
-
-      {resultado && <Resultados resultado={resultado}/>}
-
+      
+      {resultado && (<Resultados numitems={CONFIG.num_items} resultado={resultado} />)}
       
     </div>
   );
